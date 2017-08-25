2751f77b6ecd4f3acaa49e9c0fabab921cf2f921
hanguolong <724584644@qq.com>
2017-08-25 20:41:26 +0800

    添加scrollView
    Signed-off-by: hanguolong <724584644@qq.com>

--- Movie.js
+++ Movie.js
@@ -4,6 +4,64 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  ScrollView
 } from 'react-native';
 
+
+class Movie extends Component{
+	render(){
+
+		return(
+			<View style={styles.container}>
+				<ScrollView style={styles.scrollView}>
+					<View style={styles.view_1}></View>
+					<View style={styles.view_2}></View>
+					<View style={styles.view_3}></View>
+				</ScrollView>
+			</View>	
+
+		)	
+
+	}
+		
+	
+}
+
+var styles = StyleSheet.create({
+	container:{
+		flex:1,
+		backgroundColor:'cyan'
+	},
+	scrollView:{
+		marginTop:25,
+		backgroundColor:'#cccccc',
+	},
+	font:{
+		fontSize:25,
+		fontWeight:'bold',
+		textAlign:'center',
+	},
+	view_1:{
+		margin:15,
+		flex:1,
+		height:300,
+		backgroundColor:'yellow'
+	},
+	view_2:{
+		margin:15,
+		flex:1,
+		height:300,
+		backgroundColor:'green'
+	},
+	view_3:{
+		margin:15,
+		flex:1,
+		height:300,
+		backgroundColor:'red'
+	},
+})
+
+
+
+export default Movie;
--- index.ios.js
+++ index.ios.js
@@ -17,14 +17,16 @@ import Header from './header';
 import News from './newsContent';
 import Fuck from './fuck'; 
 import HanImage from './myImage';
+import Movie from './Movie';
+
 
 let someNews = ['金三胖','特朗普发推','习大大万岁!']; 
 
 function NewHeader (){
   return(
-  	<View >
-      <HanImage />
-  	</View>
+  	 
+      <Movie />
+  	 
     )
 }
   
